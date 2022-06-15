0824c180fa56f9d3d09a4f6a719927ac3faaeb71
Debox-Mehank <mehank@debox.co.in>
2022-06-15 22:42:52 +0530

    permission changes

--- src/utils/permissions.ts
+++ src/utils/permissions.ts
@@ -1,19 +1,29 @@
 import { ApolloError } from "apollo-server";
 import { MiddlewareFn } from "type-graphql";
-import { UserRole } from "../schema/user.schema";
+import { UserRole, UserStatus } from "../schema/user.schema";
 import Context from "../types/context";
+import { getUserById } from "./helper";
 
 export const isAuth: MiddlewareFn<Context> = async ({ context }, next) => {
-    if (!context.user) {
-        throw new ApolloError("You are not authenticated!")
-    }
-    return await next()
+  if (!context.user) {
+    throw new ApolloError("You are not authenticated!");
+  }
+
+  const user = await getUserById(context.user);
+  if (user?.userStatus === UserStatus.blockedByAdmin) {
+    throw new ApolloError("Your account is blocked!");
+  }
+
+  return await next();
 };
 
 export const isAdmin: MiddlewareFn<Context> = async ({ context }, next) => {
-    if (context.role === UserRole.employee || context.role === UserRole.employer) {
-        throw new ApolloError("You are not authorized!")
-    }
+  if (
+    context.role === UserRole.employee ||
+    context.role === UserRole.employer
+  ) {
+    throw new ApolloError("You are not authorized!");
+  }
 
-    return await next()
+  return await next();
 };
